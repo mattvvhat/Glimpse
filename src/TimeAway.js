f25410eb1d81feddefe5f5a63d3c723e0ab9a50c
Matt Derasoirs <matt@planwork.us>
2014-03-08 16:40:06 -0500

    Finished concept of class
    Now:
    
    * Minification and `Gruntfile`
    * Support CommonJS and AMD
    * Write `bower.json`
    * Write `component.json`
    
    Maybe
    
    * Make node-friendly
    * Write `package.json`

---
+++ src/TimeAway.js
@@ -0,0 +1,71 @@
+/**
+ *
+ *
+ */
+
+
+/**
+ * Helper Class to Figure Some Things Out
+ *
+ *
+ *
+ */
+function TimeAway (domEl) {
+
+  if (!(this instanceof TimeAway)) {
+    return new TimeAway(domEl);
+  }
+
+  if (typeof domEl === 'undefined') {
+    domEl = window;
+  }
+
+  //
+  var self = this;
+
+  //
+  self.domEl = typeof domEl === 'string' ? document.getElementById(domEl) : domEl;
+  self.lastFocused = undefined;
+  self.lastBlurred = undefined;
+  self.lastChanged = undefined;
+  self.blurredCallback = undefined;
+  self.focusedCallback = undefined;
+
+
+  // Immediately invoke function, to create a closure
+  ~function () {
+    self.domEl.onfocus = function (ev) {
+      self.lastFocused = +new Date();
+      self.lastChanged = self.lastFocused - self.lastBlurred;
+      if (typeof self.focusedCallback === 'function') {
+        self.focusedCallback.call(self, ev);
+      }
+    };
+    self.domEl.onblur = function (ev) {
+      self.lastBlurred = +new Date();
+      self.lastChanged = self.lastBlurred - self.lastFocused ;
+      if (typeof self.blurredCallback === 'function')
+        self.blurredCallback.call(self, ev);
+    };
+  }();
+
+  return self;
+}
+
+TimeAway.prototype.blurred = function (callback) {
+  if (typeof callback === 'function') {
+    this.blurredCallback = callback;
+  }
+  return this;
+};
+
+/**
+ *
+ *
+ */
+TimeAway.prototype.focused = function (callback) {
+  if (typeof callback === 'function') {
+    this.focusedCallback = callback;
+  }
+  return this;
+};
